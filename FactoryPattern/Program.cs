13f6720065ec976dabe28d2c2c95fa11ffc03c79
justin long <justinxlong93@gmail.com>
2025-01-17 16:27:35 -0600

    Added notes

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //Complete the console app to utilize factory pattern by taking a user’s input of how many tires are on a vehicle and,
+            //based on their input, creates that type of vehicle. Complete this using an interface.
+            //You must have at least 2 subclasses. For instance, if I type 4 into the console, the program would create a car or truck,
+            //or if someone types 2 it would create a motorcycle.
+
+            //Create an Interface named IVehicle that has a stubbed out public void Drive method.
+            //Create 2 new public classes that will conform to IVehicle.
+            //Example) Car, Motorcycle, BigRig
+
+            //Note: These classes must conform to IVehicle and implement the Drive() method - which will just Console.WriteLine(“Building a new Car!”)
+
+            //Now we will make our static VehicleFactory class. It will contain a static method GetVehicle(),
+            //that will return an IVehicle based on the amount of tires it’s given as a parameter Call this functionality in the Main method.
         }
     }
 }
